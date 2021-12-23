d8725625c946cad7c5121e689559b098dcbf14df
Skkomolkina <skomolkina@gmail.com>
2021-12-23 14:50:54 +0300

    add: slider section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './index.css';
+import Swiper, { Scrollbar } from "swiper";
 
-
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    loop: true,
+    modules: [Scrollbar],
+    // freeMode: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+        draggable: true,
+        dragSize: 3,
+    },
+});
