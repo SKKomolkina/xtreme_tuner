35aeda898593acd6a104ea48738268b13d2e720c
Skkomolkina <skomolkina@gmail.com>
2021-12-23 22:14:57 +0300

    update: swiper

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import './index.css';
-import Swiper, { Scrollbar } from "swiper";
+import Swiper, { Scrollbar, Keyboard, Mousewheel } from "swiper";
+
+const oneCLickButton = document.querySelector('#one');
+const rgbButton = document.querySelector('#rgb');
+const fanButton = document.querySelector('#fan');
+const monitoringButton = document.querySelector('#monitoring');
+
 
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: 2,
     loop: true,
-    modules: [Scrollbar],
+    modules: [Scrollbar, Keyboard, Mousewheel],
     // freeMode: true,
     scrollbar: {
         el: ".swiper-scrollbar",
         hide: false,
         draggable: true,
-        dragSize: 3,
+        dragSize: 249,
+        // dragClass: '.swiper-scrollbar-drag',
     },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+    mousewheel: {
+        invert: false,
+    },
+    breakpoints: {
+        1300: {
+            slidesPerView: 1.5,
+        }
+    }
 });
